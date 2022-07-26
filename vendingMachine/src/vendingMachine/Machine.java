e22d54484f8c2cd8d23f24f9572e287f76bb08cb
LemmWinks <jomsoutosantiago@gmail.com>
2022-07-26 20:14:19 -0300

    added getters, setters and constructor to machine

--- vendingMachine/src/vendingMachine/Machine.java
+++ vendingMachine/src/vendingMachine/Machine.java
@@ -2,4 +2,24 @@ package vendingMachine;
 
 public class Machine {
 
+	Item[][] Items;
+
+	public Item getItems(int row, int spot) {
+		return new Item(this.Items[row][spot]);
+	}
+
+	public void setItems(Item item, int row, int spot) {
+		this.Items[row][spot] = new Item(item);
+	}
+	
+	public Machine(Item[][] items) {
+		this.Items = new Item[items.length][items[0].length];
+		
+		for (int i = 0; i < items.length; i++) {
+			for (int j = 0; j < items.length; j++) {
+				this.Items[i][j] = new Item(items[i][j]);
+			}
+		}
+	}
+	
 }
--- vendingMachine/src/vendingMachine/Main.java
+++ vendingMachine/src/vendingMachine/Main.java
@@ -1,10 +1,31 @@
 package vendingMachine;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\t************************************************");
+        System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
+        System.out.println("\t************************************************");
+
+        Item[][] items = new Item[][] {
+            { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+            { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
+            { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
+        };
+        
+        Machine machine = new Machine(items);
+        
+        Item item = machine.getItems(2, 1);
+        item.setPrice(2.99);
+        
+        System.out.println(item);
+        
+        scan.close();
 
 	}
 
