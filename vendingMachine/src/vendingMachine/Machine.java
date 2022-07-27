da84cd14daece35ebca895271f5affd0ad87b4ce
LemmWinks <jomsoutosantiago@gmail.com>
2022-07-27 10:48:51 -0300

    updated machine.java
    just made it a little more readable

--- vendingMachine/src/vendingMachine/Machine.java
+++ vendingMachine/src/vendingMachine/Machine.java
@@ -23,15 +23,15 @@ public class Machine {
 	}
 	
     /**
-     * Function name -- dispense
+     * Method name: dispense
      * @param row (int)
      * @param spot (int)
      * @return (boolean)
      *
-     * Inside the function:
+     * Inside the method:
      *  1. Checks if the requested item has a quantity bigger than 0.
-     * - if so: decreases its quantity by one and returns true.
-     * - otherwise: returns false.
+     * 		- if so: decreases its quantity by one and returns true.
+     * 		- otherwise: returns false.
      */
 	
 	public boolean dispense(int row, int spot) {
